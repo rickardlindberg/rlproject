f9d99e839ba8cddf09cf4b9d3d6d83dc3af4ebea
Rickard Lindberg <rickard@rickardlindberg.me>
2022-11-10 11:34:08 +0100

    Add line numbers.

--- rlprojectlib/projections/lines_to_terminal_text.py
+++ rlprojectlib/projections/lines_to_terminal_text.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 from rlprojectlib.domains.string import String
 from rlprojectlib.domains.lines import Lines
 from rlprojectlib.domains.generic import Selections
-from rlprojectlib.domains.terminaltext import TerminalText, TerminalTextProjection
+from rlprojectlib.domains.terminaltext import TerminalText, TerminalTextProjection, TerminalTextFragment
 from rlprojectlib.projections.string_to_terminal_text import StringToTerminalText
 
 class LinesToTerminalText(
@@ -18,15 +18,17 @@ class LinesToTerminalText(
         ...     Lines(lines=("one", "two"), selections=Selections([]))
         ... ).terminal_text
         >>> tt.fragments
-        [TerminalTextFragment(x=0, y=0, text='one', bold=None, bg=None, fg=None), TerminalTextFragment(x=0, y=1, text='two', bold=None, bg=None, fg=None)]
+        [TerminalTextFragment(x=0, y=0, text='1', bold=None, bg=None, fg=None), TerminalTextFragment(x=2, y=0, text='one', bold=None, bg=None, fg=None), TerminalTextFragment(x=0, y=1, text='2', bold=None, bg=None, fg=None), TerminalTextFragment(x=2, y=1, text='two', bold=None, bg=None, fg=None)]
         >>> tt.cursors
         []
         """
         fragments = []
         cursors = []
+        line_number_len = len(str(len(lines.lines)))
         for index, string in enumerate(lines.lines):
             y = String(string=string, selections=tuple())
-            x = StringToTerminalText.project(y, y=index)
+            x = StringToTerminalText.project(y, y=index, x=line_number_len+1)
+            fragments.append(TerminalTextFragment(x=0, y=index, text=str(index+1)))
             fragments.extend(x.fragments)
             #cursors.extend(x.cursors)
             # TODO: fixme!
