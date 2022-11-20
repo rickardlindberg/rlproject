6d153aa9917540acb3820114536960bc57b73510
Rickard Lindberg <rickard@rickardlindberg.me>
2022-11-20 10:26:26 +0100

    Editor can show and hide filter popup.

--- rlprojectlib/projections/terminal/editor.py
+++ rlprojectlib/projections/terminal/editor.py
@@ -8,12 +8,12 @@ from rlprojectlib.domains.terminal import TextFragment
 from rlprojectlib.domains.terminal import Projection
 
 class Editor(
-    namedtuple("Editor", "projection terminal width"),
+    namedtuple("Editor", "projection terminal width popup"),
     Projection
 ):
 
     @staticmethod
-    def project(terminal, event=None, width=0, ms=0):
+    def project(terminal, event=None, width=0, ms=0, popup=None):
         """
         I project a status bar followed by the given terminal text:
 
@@ -25,8 +25,23 @@ class Editor(
         TextFragment(x=0, y=0, text='None 0ms', bold=None, bg='MAGENTA', fg='WHITE')
         Cursor(x=0, y=1)
         """
-        return Editor(
-            projection=terminal.translate(dy=1).add_fragment(
+        if popup:
+            projection = terminal.translate(dy=2).add_fragment(TextFragment(
+                text=f"{event} {ms}ms".ljust(width),
+                x=0,
+                y=0,
+                bg="MAGENTA",
+                fg="WHITE"
+            )).add_fragment(TextFragment(
+                text=f"Filter: ".ljust(width),
+                x=0,
+                y=1,
+                bg="GREEN",
+                fg="WHITE",
+                bold=True
+            ))
+        else:
+            projection = terminal.translate(dy=1).add_fragment(
                 TextFragment(
                     text=f"{event} {ms}ms".ljust(width),
                     x=0,
@@ -34,31 +49,53 @@ class Editor(
                     bg="MAGENTA",
                     fg="WHITE"
                 )
-            ),
+            )
+        return Editor(
+            projection=projection,
             terminal=terminal,
-            width=width
+            width=width,
+            popup=popup
         )
 
     def keyboard_event(self, event):
-        terminal, ms = measure_ms(lambda:
-            self.terminal.keyboard_event(event)
-        )
+        terminal = self.terminal
+        popup = self.popup
+        ms = 0
+        if self.popup:
+            if event.unicode_character == "\x07":
+                popup = None
+            else:
+                popup, ms = measure_ms(lambda:
+                    self.popup.keyboard_event(event)
+                )
+        elif event.unicode_character == "\x07":
+            popup = Terminal.create()
+        else:
+            terminal, ms = measure_ms(lambda:
+                self.terminal.keyboard_event(event)
+            )
         return Editor.project(
             terminal=terminal,
             event=event,
             width=self.width,
-            ms=ms
+            ms=ms,
+            popup=popup
         )
 
     def size_event(self, event):
+        if self.popup:
+            popup_height = 1
+        else:
+            popup_height = 0
         terminal, ms = measure_ms(lambda:
-            self.terminal.size_event(event.resize(dh=-1))
+            self.terminal.size_event(event.resize(dh=-1-popup_height))
         )
         return Editor.project(
             terminal=terminal,
             event=event,
             width=event.width,
-            ms=ms
+            ms=ms,
+            popup=self.popup
         )
 
 def measure_ms(fn):
