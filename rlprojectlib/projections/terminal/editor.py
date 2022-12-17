ca184f90b2f139c4e9f19bcb0add534a54d9bd43
Rickard Lindberg <rickard@rickardlindberg.me>
2022-12-17 08:52:48 +0100

    Render status bar in split.

--- rlprojectlib/projections/terminal/editor.py
+++ rlprojectlib/projections/terminal/editor.py
@@ -86,18 +86,18 @@ class Editor(Terminal):
     def project(document):
         """
         >>> document = String.from_string("hello").replace_meta(EditorState(
-        ...     width=10,
+        ...     width=12,
         ...     height=10,
         ...     popup=None,
         ...     event=None,
         ...     measurement_event=MeasurementEvent(0, 0)
         ... ))
         >>> Editor.project(document).print_fragments_and_cursors()
+        TextFragment(x=0, y=0, text='None 0ms 0ms', bold=None, bg='MAGENTA', fg='WHITE')
         TextFragment(x=0, y=1, text='1', bold=None, bg=None, fg='YELLOW')
         TextFragment(x=2, y=1, text='hello', bold=None, bg=None, fg=None)
-        TextFragment(x=0, y=5, text='----------', bold=None, bg='FOREGROUND', fg='BACKGROUND')
+        TextFragment(x=0, y=5, text='------------', bold=None, bg='FOREGROUND', fg='BACKGROUND')
         TextFragment(x=0, y=6, text='hello', bold=None, bg=None, fg=None)
-        TextFragment(x=0, y=0, text='None 0ms 0ms', bold=None, bg='MAGENTA', fg='WHITE')
         Cursor(x=2, y=1)
         """
         if document.meta.popup:
@@ -108,57 +108,61 @@ class Editor(Terminal):
             )
         else:
             popup_terminal = None
+        splits = []
+        splits.append(Options(
+            Terminal.create(
+                fragments=[TextFragment(
+                    text=f"{document.meta.event} {document.meta.measurement_event.ms_project}ms {document.meta.measurement_event.ms_repaint}ms".ljust(document.meta.width),
+                    x=0,
+                    y=0,
+                    bg="MAGENTA",
+                    fg="WHITE"
+                )]
+            ),
+            0,
+            False
+        ))
+        splits.append(Options(
+            LinesToTerminal.project(
+                StringToLines.project(
+                    document
+                )
+            ),
+            1,
+            True
+        ))
+        splits.append(Options(
+            Terminal.create(
+                fragments=[TextFragment(
+                    x=0,
+                    y=0,
+                    bg="FOREGROUND",
+                    fg="BACKGROUND",
+                    text="-"*document.meta.width
+                )]
+            ),
+            0,
+            False
+        ))
+        splits.append(Options(
+            StringToTerminal.project(
+                document,
+                x=0,
+                y=0
+            ),
+            1,
+            False
+        ))
         split = VSplit.project(
-            [
-                Options(
-                    LinesToTerminal.project(
-                        StringToLines.project(
-                            document
-                        )
-                    ),
-                    1,
-                    True
-                ),
-                Options(
-                    Terminal.create(
-                        fragments=[TextFragment(
-                            x=0,
-                            y=0,
-                            bg="FOREGROUND",
-                            fg="BACKGROUND",
-                            text="-"*document.meta.width
-                        )]
-                    ),
-                    0,
-                    False
-                ),
-                Options(
-                    StringToTerminal.project(
-                        document,
-                        x=0,
-                        y=0
-                    ),
-                    1,
-                    False
-                ),
-            ],
+            splits,
             height=document.meta.height,
             width=document.meta.width
         )
         terminal = split
-        status_fragment = TextFragment(
-            text=f"{document.meta.event} {document.meta.measurement_event.ms_project}ms {document.meta.measurement_event.ms_repaint}ms".ljust(document.meta.width),
-            x=0,
-            y=0,
-            bg="MAGENTA",
-            fg="WHITE"
-        )
         if popup_terminal:
             projection = terminal.clear_cursors(
             ).translate(
-                dy=2
-            ).add_fragment(
-                status_fragment
+                dy=1
             ).add_fragment(TextFragment(
                 text=f"Filter:".ljust(document.meta.width),
                 x=0,
@@ -168,11 +172,7 @@ class Editor(Terminal):
                 bold=True
             )).merge(popup_terminal.style(bg="GREEN", fg="WHITE").translate(dy=1, dx=8))
         else:
-            projection = terminal.translate(
-                dy=1
-            ).add_fragment(
-                status_fragment
-            )
+            projection = terminal
         return Editor(
             *projection.replace_meta(ProjectionState(
                 terminal=terminal,
